30f9ac77db8a6a95f1e6d8dfecddf6fd5264ce1f
KauaaGodoii <kauaramosgodoi@gmail.com>
2024-11-13 21:54:13 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,41 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Filme',
+    'De que casa Harry Potter pertence?',
+    'Harry Potter é de Grifinória'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual continente fica o Brasil?',
+    'O Brasil fica na América'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Geral',
+    'Quantas cores tem o arco-íris?',
+    'O Arco-íris tem 7 cores'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz olá em ingles?',
+    'Ola em ingles é Hello (relou)'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'Quantas sílabas tem a palavra macarrão',
+    'Tem 3 sílabas'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'O Distrito Federel fica em Brasília'
+)
+
+criaCartao(
+    'Mateatia',
+    'Quanto é 5+2x3?',
+    '11'
 )
